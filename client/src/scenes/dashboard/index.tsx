5213322ec81b1628f755a44d1c25f73da81fff80
hzein <hzein286@gmail.com>
2023-06-09 10:20:54 -0700

    First commit

---
+++ client/src/scenes/dashboard/index.tsx
@@ -0,0 +1,39 @@
+import { Box, useMediaQuery } from "@mui/material";
+import {
+  gridTemplateLargeScreens,
+  gridTemplateSmallScreens,
+} from "@/layouts/gridLayout";
+import Row1 from "./Row1";
+import Row2 from "./Row2";
+import Row3 from "./Row3";
+
+function Dashboard() {
+  const isAboveMediumScreens = useMediaQuery("(min-width: 1200px)");
+  return (
+    <Box
+      width="100%"
+      height="100%"
+      display="grid"
+      gap="1.5rem"
+      sx={
+        isAboveMediumScreens
+          ? {
+              gridTemplateColumns: "repeat(3, minmax(370px, 1fr))",
+              gridTemplateRows: "repeat(10, minmax(60px, 1fr))",
+              gridTemplateAreas: gridTemplateLargeScreens,
+            }
+          : {
+              gridAutoColumns: "1fr",
+              gridAutoRows: "80px",
+              gridTemplateAreas: gridTemplateSmallScreens,
+            }
+      }
+    >
+      <Row1 />
+      <Row2 />
+      <Row3 />
+    </Box>
+  );
+}
+
+export default Dashboard;
